9b022eda4a4ee1186bc6f701e3c8c8a0cbc52478
SPalacioAhun <spa497@gmail.com>
2025-02-13 16:16:59 -0300

    Archivo string

--- index.php
+++ index.php
@@ -7,4 +7,92 @@ echo "<h3>Edad: 25</h3>";
 <h1>Santiago</h1>
 <h2><?= $name ?></h2>
 <h2><?php echo $lastname ?></h2>
-<h3></h3>Edad: 25</h3>
+<h3>Edad: 25</h3>
+
+<h2>Tipos de datos.</h2>
+<?php
+//integer
+$x = 50;
+var_dump($x);
+?>
+
+<br>
+
+<?php 
+//string 
+$x = "Hola Mundo!";
+$y = 'Hello World!';
+
+var_dump($x);
+echo "<br>";
+var_dump($y);
+?>
+
+<br>
+
+<?php
+//float
+$x = 10.365;
+var_dump($x);
+?>
+
+<br>
+
+<?php
+//boolean
+$x = true;
+var_dump($x);
+?>
+
+<br>
+
+<?php
+#array
+$cars = array("Volvo", "BMW", "Toyota");
+var_dump($cars);
+?>
+
+<br>
+
+<?php
+#object
+class Car {
+  public $color;
+  public $model;
+  public function __construct($color, $model) {
+    $this->color = $color;
+    $this->model = $model;
+  }  
+}
+$myCar = new Car("blue", "Ford");
+var_dump($myCar);
+?>
+
+<br>
+
+<?php
+#null
+$x = "Hola MUndo!";
+$x = null;
+var_dump($x);
+?>
+
+<br>
+
+<h2>Camabiar el tipo de datos</h2>
+
+<?php
+$x = 5985;
+var_dump($x);
+echo "<br>"; 
+$x = "Hola que tal!";
+var_dump($x);
+?>
+
+<br>
+
+<?php
+$x = 5;
+$x = (array) $x;
+var_dump($x);
+?> 
