02e644dc7032633fb0f74b37e9fc5f26d74b30b7
SPalacioAhun <spa497@gmail.com>
2025-02-13 14:27:33 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+$name = "Malena";
+$lastname = "Palacio";
+echo "<h2>Nombre: $name $lastname</h2><br>";
+echo "<h3>Edad: 25</h3>";
+?>
+<h1>Santiago</h1>
+<h2><?= $name ?></h2>
+<h2><?php echo $lastname ?></h2>
+<h3></h3>Edad: 25</h3>
